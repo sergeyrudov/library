866fc77991c844d30f48e649b28ba6321990cba9
sergey <m3quattro@mail.ru>
2017-05-19 10:04:40 +0300

    added template

---
+++ albums/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^all-authors/', views.book_request),
+]
+
+
+
--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Photo, Album
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def Albums_display(request):
+    alb = Album.objects.all()
+    context = {'albums': alb }
+    return render(request, 'albums/albums.html', context)
