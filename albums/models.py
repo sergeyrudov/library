e8971003bf5ed3a006e9d6f5e5e773ff317e1245
sergey <m3quattro@mail.ru>
2017-05-19 09:13:39 +0300

    update models

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+    title = models.CharField('Name the albumx', max_length=250)
+    descr = models.TextField('Full description', blank=True)
+    date_created = models.DateTimeField('Date created',auto_now_add = True, auto_created = True)
+    date_change = models.DateTimeField('Date change', auto_now_add= True, auto_created=True)
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.title, self.descr)
+
+
+class Photo(models.Model):
+    alt = models.TextField()
+    img = models.ImageField()
+    date_created = models.DateTimeField('Date created', auto_now_add=True, auto_created=True)
+    date_change = models.DateTimeField('Date change', auto_now_add=True, auto_created=True)
+    album = models.ForeignKey(Album)
+
+    def __str__(self):
+        return self.alt
+
